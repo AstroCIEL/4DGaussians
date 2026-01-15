f17ea5319036320aa43c6e8d7c016496b3aef220
AstroCIEL <782306774@qq.com>
2026-01-15 19:37:12 +0800

    update scripts

--- scripts/train_hyper_interp.sh
+++ scripts/train_hyper_interp.sh
@@ -1,26 +1,26 @@
-export CUDA_VISIBLE_DEVICES=2&&python train.py -s data/hypernerf/interp/aleks-teapot --port 6568 --expname "hypernerf/interp/aleks-teapot" --configs arguments/hypernerf/default.py &
-export CUDA_VISIBLE_DEVICES=2&&python train.py -s data/hypernerf/interp/slice-banana --port 6566 --expname "hypernerf/interp/slice-banana" --configs arguments/hypernerf/default.py &
-export CUDA_VISIBLE_DEVICES=2&&python train.py -s data/hypernerf/interp/chickchicken --port 6569 --expname "hypernerf/interp/interp-chicken" --configs arguments/hypernerf/default.py & 
+export CUDA_VISIBLE_DEVICES=1&&python train.py -s data/hypernerf/interp/aleks-teapot --port 6568 --expname "hypernerf/interp/aleks-teapot" --configs arguments/hypernerf/default.py &
+export CUDA_VISIBLE_DEVICES=1&&python train.py -s data/hypernerf/interp/slice-banana --port 6566 --expname "hypernerf/interp/slice-banana" --configs arguments/hypernerf/default.py &
+export CUDA_VISIBLE_DEVICES=1&&python train.py -s data/hypernerf/interp/chickchicken --port 6569 --expname "hypernerf/interp/interp-chicken" --configs arguments/hypernerf/default.py & 
 
 wait
 export CUDA_VISIBLE_DEVICES=0&&python train.py -s data/hypernerf/interp/cut-lemon1 --port 6670 --expname hypernerf/interp/cut-lemon1 --configs arguments/hypernerf/default.py &
 export CUDA_VISIBLE_DEVICES=0&&python train.py -s data/hypernerf/interp/hand1-dense-v2 --port 6671 --expname hypernerf/interp/hand1-dense-v2 --configs arguments/hypernerf/default.py &
-export CUDA_VISIBLE_DEVICES=2&&python train.py -s data/hypernerf/interp/torchocolate --port 6672 --expname hypernerf/interp/torchocolate --configs arguments/hypernerf/default.py &
+export CUDA_VISIBLE_DEVICES=1&&python train.py -s data/hypernerf/interp/torchocolate --port 6672 --expname hypernerf/interp/torchocolate --configs arguments/hypernerf/default.py &
 wait
 export CUDA_VISIBLE_DEVICES=0&&python render.py --model_path output/hypernerf/interp/aleks-teapot --configs arguments/hypernerf/default.py --skip_train &
-export CUDA_VISIBLE_DEVICES=2&&python render.py --model_path output/hypernerf/interp/slice-banana  --configs arguments/hypernerf/default.py --skip_train &
+export CUDA_VISIBLE_DEVICES=1&&python render.py --model_path output/hypernerf/interp/slice-banana  --configs arguments/hypernerf/default.py --skip_train &
 export CUDA_VISIBLE_DEVICES=0&&python render.py --model_path output/hypernerf/interp/interp-chicken --configs arguments/hypernerf/default.py --skip_train &
 wait
 export CUDA_VISIBLE_DEVICES=0&&python render.py --model_path output/hypernerf/interp/cut-lemon1  --configs arguments/hypernerf/default.py --skip_train &
-export CUDA_VISIBLE_DEVICES=2&&python render.py --model_path output/hypernerf/interp/hand1-dense-v2  --configs arguments/hypernerf/default.py --skip_train&
+export CUDA_VISIBLE_DEVICES=1&&python render.py --model_path output/hypernerf/interp/hand1-dense-v2  --configs arguments/hypernerf/default.py --skip_train&
 export CUDA_VISIBLE_DEVICES=0&&python render.py --model_path output/hypernerf/interp/torchocolate --configs arguments/hypernerf/default.py --skip_train &
 
 wait
 export CUDA_VISIBLE_DEVICES=0&&python metrics.py --model_path "output/hypernerf/interp/aleks-teapot/"  &
-export CUDA_VISIBLE_DEVICES=2&&python metrics.py --model_path "output/hypernerf/interp/slice-banana/" &
+export CUDA_VISIBLE_DEVICES=1&&python metrics.py --model_path "output/hypernerf/interp/slice-banana/" &
 export CUDA_VISIBLE_DEVICES=0&&python metrics.py --model_path "output/hypernerf/interp/interp-chicken/" 
 export CUDA_VISIBLE_DEVICES=0&&python metrics.py --model_path "output/hypernerf/interp/cut-lemon1/" &
-export CUDA_VISIBLE_DEVICES=2&&python metrics.py --model_path "output/hypernerf/interp/hand1-dense-v2/" &
+export CUDA_VISIBLE_DEVICES=1&&python metrics.py --model_path "output/hypernerf/interp/hand1-dense-v2/" &
 export CUDA_VISIBLE_DEVICES=0&&python metrics.py --model_path "output/hypernerf/interp/torchocolate/" 
 wait
 echo "Done"
--- scripts/train_hyper_virg.sh
+++ scripts/train_hyper_virg.sh
@@ -1,17 +1,16 @@
-exp_name=$1
-export CUDA_VISIBLE_DEVICES=0&&python train.py -s data/hypernerf/virg/broom2 --port 6068 --expname "$exp_name/broom2" --configs arguments/$exp_name/broom2.py &
-export CUDA_VISIBLE_DEVICES=2&&python train.py -s data/hypernerf/virg/vrig-3dprinter --port 6066 --expname "$exp_name/3dprinter" --configs arguments/$exp_name/3dprinter.py &
-export CUDA_VISIBLE_DEVICES=2&&python train.py -s data/hypernerf/virg/peel-banana --port 6069 --expname "$exp_name/peel-banana" --configs arguments/$exp_name/banana.py  &
-export CUDA_VISIBLE_DEVICES=0&&python train.py -s data/hypernerf/virg/vrig-chicken --port 6070 --expname "$exp_name/vrig-chicken" --configs arguments/$exp_name/chicken.py 
+export CUDA_VISIBLE_DEVICES=1&&python train.py -s data/hypernerf/vrig/broom2 --port 6068 --expname "hypernerf/vrig/broom2" --configs arguments/hypernerf/broom2.py &
+export CUDA_VISIBLE_DEVICES=2&&python train.py -s data/hypernerf/vrig/vrig-3dprinter --port 6066 --expname "hypernerf/vrig/vrig-3dprinter" --configs arguments/hypernerf/3dprinter.py &
+export CUDA_VISIBLE_DEVICES=2&&python train.py -s data/hypernerf/vrig/vrig-peel-banana --port 6069 --expname "hypernerf/vrig/vrig-peel-banana" --configs arguments/hypernerf/banana.py  &
+export CUDA_VISIBLE_DEVICES=1&&python train.py -s data/hypernerf/vrig/vrig-chicken --port 6070 --expname "hypernerf/vrig/vrig-chicken" --configs arguments/hypernerf/chicken.py 
 wait
-export CUDA_VISIBLE_DEVICES=0&&python render.py --model_path output/$exp_name/broom2 --configs arguments/$exp_name/broom2.py --skip_train  --skip_test &
-export CUDA_VISIBLE_DEVICES=2&&python render.py --model_path output/$exp_name/3dprinter  --configs arguments/$exp_name/3dprinter.py --skip_train  --skip_test &
-export CUDA_VISIBLE_DEVICES=2&&python render.py --model_path output/$exp_name/peel-banana --configs arguments/$exp_name/banana.py --skip_train --skip_test &
-export CUDA_VISIBLE_DEVICES=0&&python render.py --model_path output/$exp_name/vrig-chicken  --configs arguments/$exp_name/chicken.py --skip_train --skip_test &
+export CUDA_VISIBLE_DEVICES=0&&python render.py --model_path output/hypernerf/broom2 --configs arguments/hypernerf/broom2.py --skip_train  --skip_test &
+export CUDA_VISIBLE_DEVICES=2&&python render.py --model_path output/hypernerf/vrig-3dprinter  --configs arguments/hypernerf/3dprinter.py --skip_train  --skip_test &
+export CUDA_VISIBLE_DEVICES=2&&python render.py --model_path output/hypernerf/vrig-peel-banana --configs arguments/hypernerf/banana.py --skip_train --skip_test &
+export CUDA_VISIBLE_DEVICES=0&&python render.py --model_path output/hypernerf/vrig-chicken  --configs arguments/hypernerf/chicken.py --skip_train --skip_test &
 wait
-export CUDA_VISIBLE_DEVICES=0&&python metrics.py --model_path "output/$exp_name/broom2/"  &
-export CUDA_VISIBLE_DEVICES=2&&python metrics.py --model_path "output/$exp_name/3dprinter/" &
-export CUDA_VISIBLE_DEVICES=2&&python metrics.py --model_path "output/$exp_name/peel-banana/" &
-export CUDA_VISIBLE_DEVICES=0&&python metrics.py --model_path "output/$exp_name/vrig-chicken/" &
+export CUDA_VISIBLE_DEVICES=0&&python metrics.py --model_path "output/hypernerf/broom2/"  &
+export CUDA_VISIBLE_DEVICES=2&&python metrics.py --model_path "output/hypernerf/3dprinter/" &
+export CUDA_VISIBLE_DEVICES=2&&python metrics.py --model_path "output/hypernerf/peel-banana/" &
+export CUDA_VISIBLE_DEVICES=0&&python metrics.py --model_path "output/hypernerf/vrig-chicken/" &
 wait
 echo "Done"
